046cc79cff7c10bed35ee7f6d84943aae02414c8
Al-hafiz Academy App <itsranas517@gmail.com>
2021-07-12 02:43:13 -0700

    Create Navbar & Footer using materialize css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,9 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+//= require rails-ujs
+//= require materialize
+//= require material_icons
+//= require turbolinks
+//= require_tree .
